49d5c43b512ccdd3e6c021218f91156112ffbbf8
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-07-13 22:39:14 +0530

    (feature) Add basic table

---
+++ src/components/columns.js
@@ -0,0 +1,34 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'First Name',
+        accessor: 'first_name'
+    },
+    {
+        Header: 'Last Name',
+        accessor: 'last_name'
+    },
+    {
+        Header: 'Email',
+        accessor: 'email'
+    },
+    {
+        Header: 'Date Of Birth',
+        accessor: 'date_of_birth'
+    },
+    {
+        Header: 'Age',
+        accessor: 'age'
+    },
+    {
+        Header: 'Country',
+        accessor: 'country'
+    },
+    {
+        Header: 'Phone',
+        accessor: 'phone'
+    },
+]
